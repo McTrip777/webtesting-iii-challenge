4a90225a321e993ee094aa6aa49fafb5a3c62f67
Jacob McFaul <mcjakedude@gmail.com>
2019-04-10 11:37:32 -0700

    Installed yarn and created snapshot

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,15 @@
 // Test away
+import React from 'react';
+import { render, fireEvent } from 'react-testing-library';
+import renderer from 'react-test-renderer';
+import 'react-testing-library/cleanup-after-each';
+import Dashboard from './Dashboard';
+
+describe('<App />', () => {
+    it('matches snapshot', () => {
+      const tree = renderer.create(<Dashboard />).toJSON();
+      expect(tree).toMatchSnapshot();
+    });
+  
+    // idea for another test
+  });
